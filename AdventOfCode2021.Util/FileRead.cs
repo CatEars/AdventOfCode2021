01a066f5c7f790cc176564b90d140ccd3ee3ebd6
Henke <catears13@gmail.com>
2021-12-01 07:34:02 +0100

    Solve the first two stars of AdventOfCode

---
+++ AdventOfCode2021.Util/FileRead.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021.Util
+{
+    public static class FileRead
+    {
+
+        public static List<int> ReadIntList(string filepath)
+        {
+            return File.ReadLines(filepath)
+                .Select(x => int.Parse(x))
+                .ToList();
+        }
+        
+    }
+}
